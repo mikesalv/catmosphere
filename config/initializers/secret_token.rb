5b7e4ed9a47db93167d17693ba38fb43e250103a
Mike Salvaris <mike_salv1@hotmail.com>
2014-04-27 14:18:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catmosphere::Application.config.secret_key_base = 'bfb1da61590f32cffbf0a901b6071202b80e3c798d66695baf37d1cf0311e6ec51e5fb2b8135b45e8037cca0dfb0d701d083a1a1997d6195ef3800d9b7826bc9'
